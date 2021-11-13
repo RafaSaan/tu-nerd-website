4fb4026beafa5e56f3d9e194efbd4fc15d6c8a68
rafa <rafasaanchez@icloud.com>
2021-11-12 18:58:50 -0600

    add router pages

---
+++ src/pages/PageNotFound.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="not-found">
+    <img src="../assets/images/404.png" alt="error 404" />
+    <h3>Page not found</h3>
+    <p>La página que estás buscando no existe o ha ocurrido un error.</p>
+    <router-link exact to="/">
+      Regresar a <span class="not-found--link">Tu Nerd</span>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "pageNotFound",
+};
+</script>
+
+<style>
+.not-found {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: #000;
+}
+.not-found > img,
+h3,
+p {
+  margin-bottom: 1.5rem;
+}
+
+.not-found > h3 {
+  margin-bottom: 1.5rem;
+  font-size: var(--step-2);
+}
+.not-found > p {
+  margin: 0 1rem 1.5rem 1.5rem;
+  /* margin-bottom: 1.5rem; */
+  font-size: var(--step--1);
+  /* width: 80%; */
+}
+.not-found > a {
+  color: #000;
+}
+.not-found--link {
+  color: var(--blue);
+  text-decoration: none;
+}
+</style>
---
+++ src/pages/TermsAndConditionsPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="t-a-c">
+    <router-link to="/"> regresar a Tu Nerd</router-link>
+    <p class="P1">TERMINOS Y CONDICIONES LEGALES</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.t-a-c {
+  width: 80%;
+}
+</style>
